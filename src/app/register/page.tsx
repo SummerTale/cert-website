d99a76f6eab5327256cf258643d5830ba0bd1c68
Kavya Verma <kavyaverma5601@gmail.com>
2025-10-23 22:59:26 -0400

    Vercel fix -- force dynamic

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -7,6 +7,8 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { useSearchParams } from "next/navigation";
 
+export const dynamic = 'force-dynamic';
+
 export default function RegisterPage() {
 
   const searchParams = useSearchParams();
